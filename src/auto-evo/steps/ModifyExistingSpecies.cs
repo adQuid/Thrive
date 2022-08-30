6c626171cd7603e67a6504e2cbfdc4746d128372
adQuid <28126232+adQuid@users.noreply.github.com>
2022-08-30 18:17:29 -0400

    add basic mutations

---
+++ src/auto-evo/steps/ModifyExistingSpecies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoEvo;
+
+class ModifyExistingSpecies : IRunStep
+{
+    public Species Species;
+
+    public ModifyExistingSpecies(Species species)
+    {
+        Species = species;
+    }
+
+    public int TotalSteps => 1;
+
+    public bool CanRunConcurrently => true;
+
+    public bool RunStep(RunResults results)
+    {
+        var variantSpecies = (MicrobeSpecies)Species.Clone();
+
+        variantSpecies.Colour = new Godot.Color(255.0f, 0.0f, 255.0f);
+
+        results.AddMutationResultForSpecies(Species, variantSpecies);
+        return true;
+    }
+}
