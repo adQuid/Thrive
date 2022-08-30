f0b9968ea5669132b8ba6f9053837ec34c60e337
adQuid <28126232+adQuid@users.noreply.github.com>
2022-08-30 14:08:09 -0400

    setup basic structure

---
+++ src/auto-evo/simulation/MutationStrategy/IMutationStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IMutationStrategy<T> where T: Species
+{
+    public abstract List<T> MutationsOf(T baseSpecies);
+}
---
+++ src/auto-evo/simulation/selection_pressure/SelectionPressure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoEvo;
+
+abstract class SelectionPressure
+{
+    public float Strength;
+    public List<IMutationStrategy<MicrobeSpecies>> MicrobeMutations;
+    public List<IMutationStrategy<EarlyMulticellularSpecies>> MulticellularMutations;
+
+    public SelectionPressure(float Strength, List<IMutationStrategy<MicrobeSpecies>> MicrobeMutations, List<IMutationStrategy<EarlyMulticellularSpecies>> MulticellularMutations)
+    {
+        this.Strength = Strength;
+        this.MicrobeMutations = MicrobeMutations;
+        this.MulticellularMutations = MulticellularMutations;
+    }
+
+    public abstract float Score(Species species, SimulationCache cache);
+}
