2ad3e55033bfd76ad4f2dbf33d24651b7b0c804a
adQuid <28126232+adQuid@users.noreply.github.com>
2022-08-30 17:54:52 -0400

    add exclusive variable

--- src/auto-evo/simulation/selection_pressure/SelectionPressure.cs
+++ src/auto-evo/simulation/selection_pressure/SelectionPressure.cs
@@ -3,12 +3,18 @@ using AutoEvo;
 
 public abstract class SelectionPressure
 {
+    /// <summary>
+    ///   A species will be eliminated by another if it is outcompeted in every exclusive pressure by another species
+    /// </summary>
+    public bool Exclusive;
+
     public float Strength;
     public List<IMutationStrategy<MicrobeSpecies>> MicrobeMutations;
     public List<IMutationStrategy<EarlyMulticellularSpecies>> MulticellularMutations;
 
-    public SelectionPressure(float strength, List<IMutationStrategy<MicrobeSpecies>> microbeMutations, List<IMutationStrategy<EarlyMulticellularSpecies>> multicellularMutations)
+    public SelectionPressure(bool exclusive, float strength, List<IMutationStrategy<MicrobeSpecies>> microbeMutations, List<IMutationStrategy<EarlyMulticellularSpecies>> multicellularMutations)
     {
+        Exclusive = exclusive;
         Strength = strength;
         MicrobeMutations = microbeMutations;
         MulticellularMutations = multicellularMutations;
