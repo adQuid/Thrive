30aaac0087eb8184be06858f2a91f56defdb4a4c
adQuid <28126232+adQuid@users.noreply.github.com>
2022-09-01 20:18:01 -0400

    add pruning logic

--- src/auto-evo/steps/ModifyExistingSpecies.cs
+++ src/auto-evo/steps/ModifyExistingSpecies.cs
@@ -5,15 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoEvo;
 
-class ModifyExistingSpecies : IRunStep
+public class ModifyExistingSpecies : IRunStep
 {
     public Species Species;
     public Patch Patch;
+    public SimulationCache Cache;
 
-    public ModifyExistingSpecies(Species species, Patch patch)
+    public ModifyExistingSpecies(Species species, Patch patch, SimulationCache cache)
     {
         Species = species;
         Patch = patch;
+        Cache = cache;
     }
 
     public int TotalSteps => 1;
@@ -22,20 +24,63 @@ class ModifyExistingSpecies : IRunStep
 
     public bool RunStep(RunResults results)
     {
-        var variantSpecies = (MicrobeSpecies)Species.Clone();
+        var modifiedSpecies = (MicrobeSpecies)Species.Clone();
 
         var selectionPressures = new List<SelectionPressure>
         {
-            new OsmoregulationEfficiencyPressure(Patch, 10.0f),
-            new AutotrophEnergyEfficiencyPressure(Patch, 0.8f),
+            new AutotrophEnergyEfficiencyPressure(Patch, 10.0f),
+            new OsmoregulationEfficiencyPressure(Patch, 5.0f),
         };
+
+        // find the initial scores
+        var pressureScores = new Dictionary<SelectionPressure, float>();
+        foreach (var curPressure in selectionPressures)
+        {
+            pressureScores[curPressure] = curPressure.Score(modifiedSpecies, Cache);
+        }
+
+        var viableVariants = new List<MicrobeSpecies> { modifiedSpecies };
 
         foreach (var curPressure in selectionPressures)
         {
-            variantSpecies = curPressure.MicrobeMutations.First().MutationsOf(variantSpecies).First();
+            var previousPressures = selectionPressures.GetRange(0, selectionPressures.IndexOf(curPressure) - 1);
+            previousPressures.Reverse();
+
+            // For each viable variant, get a new variants that at least improve score a little bit
+            var potentialVariants = viableVariants.Select(x =>
+                curPressure.MicrobeMutations.First().MutationsOf(x).Where(x => curPressure.Score(x, Cache) > pressureScores[curPressure])).SelectMany(x => x);
+
+            // Clear the last round's variants
+            viableVariants = new List<MicrobeSpecies> { modifiedSpecies };
+
+            // Prune variants that don't hurt the previous scores too much
+            foreach (var potentialVariant in potentialVariants)
+            {
+                var currentImprovement = curPressure.Score(potentialVariant, Cache) / pressureScores[curPressure];
+
+                var viable = true;
+                foreach (var pastPressure in previousPressures)
+                {
+                    var pastImprovement = pastPressure.Score(potentialVariant, Cache) / pressureScores[pastPressure];
+
+                    // If, proportional to weights, the improvement to the current pressure doesn't outweigh the loss to the other pressures, this is still viable.
+                    if (currentImprovement * curPressure.Strength + pastImprovement * pastPressure.Strength < curPressure.Strength + pastPressure.Strength)
+                    {
+                        viable = false;
+                    }
+                }
+
+                if (viable)
+                {
+                    viableVariants.Add(potentialVariant);
+                }
+            }
         }
 
-        results.AddMutationResultForSpecies(Species, variantSpecies);
+        // TODO: pass this in
+        var random = new Random();
+
+        results.AddMutationResultForSpecies(Species, viableVariants[random.Next(viableVariants.Count())]);
         return true;
     }
 }
