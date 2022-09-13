b0f167309ddb98bae58d4ed802117f4cd1e3d3b4
adQuid <28126232+adQuid@users.noreply.github.com>
2022-09-13 16:34:42 -0400

    add basics of pull species for patch

---
+++ src/auto-evo/steps/PullSpeciesForPatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoEvo;
+
+class PullSpeciesForPatch : IRunStep
+{
+    public Patch Patch;
+    public SimulationCache Cache;
+
+    public int TotalSteps => 1;
+
+    public bool CanRunConcurrently => false;
+
+    public PullSpeciesForPatch(Patch patch, SimulationCache cache)
+    {
+        Patch = patch;
+        Cache = cache;
+    }
+
+    public bool RunStep(RunResults results)
+    {
+        foreach (var species in Patch.SpeciesInPatch.Keys)
+        {
+            var variants = ModifyExistingSpecies.ViableVariants(species, Patch, Cache);
+
+            results.AddNewSpecies(
+                variants.First(),
+                new[]
+                {
+                    new KeyValuePair<Patch, long>(Patch, 100),
+                },
+                RunResults.NewSpeciesType.FillNiche,
+                species
+            );
+        }
+
+        return true;
+    }
+}
