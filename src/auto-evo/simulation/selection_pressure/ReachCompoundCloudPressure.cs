80bc705df65ee5c5c0754b98784de8d471874596
adQuid <28126232+adQuid@users.noreply.github.com>
2022-10-24 19:52:48 -0400

    add pressure to actually reach compounds

---
+++ src/auto-evo/simulation/selection_pressure/ReachCompoundCloudPressure.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AutoEvo;
+
+public class ReachCompoundCloudPressure : SelectionPressure
+{
+    public ReachCompoundCloudPressure(float weight) : base(false,
+        weight,
+        new List<IMutationStrategy<MicrobeSpecies>> {
+            new LowerRigidity(),
+            new ChangeMembraneType(SimulationParameters.Instance.GetMembrane("single")),
+        },
+        new List<IMutationStrategy<EarlyMulticellularSpecies>>()
+        )
+    {
+
+    }
+
+    public override string Name()
+    {
+        return "Reach Compound Cloud";
+    }
+
+    public override float Score(Species species, SimulationCache cache)
+    {
+        var microbeSpecies = (MicrobeSpecies)species;
+
+        return microbeSpecies.BaseSpeed;
+    }
+}
