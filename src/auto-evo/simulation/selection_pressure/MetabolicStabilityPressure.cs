726d501298bf0b2aafa8b622c92f057602b509af
adQuid <28126232+adQuid@users.noreply.github.com>
2022-11-07 19:47:58 -0500

    add stability pressure

---
+++ src/auto-evo/simulation/selection_pressure/MetabolicStabilityPressure.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using AutoEvo;
+
+class MetabolicStabilityPressure : SelectionPressure
+{
+    private Patch Patch;
+
+    public MetabolicStabilityPressure(Patch patch, float weight) : base(false,
+        weight,
+        new List<IMutationStrategy<MicrobeSpecies>>
+        {
+            new AddOrganelleAnywhere(SimulationParameters.Instance.GetOrganelleType("metabolosome"))
+        },
+        new List<IMutationStrategy<EarlyMulticellularSpecies>>()
+        )
+    {
+        Patch = patch;
+    }
+
+    public override string Name()
+    {
+        return "Metabolic Stability";
+    }
+
+    public override float Score(Species species, SimulationCache cache)
+    {
+        var microbeSpecies = (MicrobeSpecies)species;
+        var energyBalance = MicrobeInternalCalculations.ComputeEnergyBalance(microbeSpecies.Organelles, Patch.Biome, microbeSpecies.MembraneType);
+
+        if (energyBalance.FinalBalance > 0)
+        {
+            return 1.0f;
+        } 
+        else if (energyBalance.FinalBalanceStationary >= 0)
+        {
+            return 0.5f;
+        }
+        else
+        {
+            return 0.0f;
+        }
+    }
+}
