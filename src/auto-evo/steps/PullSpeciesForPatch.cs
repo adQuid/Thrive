ac827c3df473657610656689a6a652ad264e474f
adQuid <28126232+adQuid@users.noreply.github.com>
2023-02-01 11:40:16 -0500

    seperate native and foreign species

--- src/auto-evo/steps/PullSpeciesForPatch.cs
+++ src/auto-evo/steps/PullSpeciesForPatch.cs
@@ -23,7 +23,7 @@ class PullSpeciesForPatch : IRunStep
 
     public bool RunStep(RunResults results)
     {
-        var variants = CandiateSpecies().ToList();
+        var variants = NeighboringSpecies().ToList();
 
         PopulateForMiche(Patch, results.MicheByPatch[Patch], variants, results, Cache);
 
@@ -33,11 +33,12 @@ class PullSpeciesForPatch : IRunStep
     /*
      * Finds or creates a species for every leaf node of the provided miche.
      */
-    private static void PopulateForMiche(Patch patch, Miche miche, IEnumerable<Species> allSpecies, RunResults results, SimulationCache cache)
+    private static void PopulateForMiche(Patch patch, Miche miche, IEnumerable<Species> foreignSpecies, RunResults results, SimulationCache cache)
     {
-        foreach (var curSpecies in allSpecies)
+        // Try to add native species to any new openings
+        // TODO: Would this ever do anything?
+        foreach (var curSpecies in patch.SpeciesInPatch.Select(x => x.Key))
         {
-            // Try to add existing species
             if (miche.Root().InsertSpecies(curSpecies))
             {
 
@@ -51,36 +52,79 @@ class PullSpeciesForPatch : IRunStep
             }
         }
 
-        FillEmptyMiches(allSpecies, results, patch, cache);
+        // Then outside species have a chance to migrate in
+        foreach (var curSpecies in foreignSpecies)
+        {
+            if (miche.Root().InsertSpecies(curSpecies))
+            {
+
+                results.MakeSureResultExistsForSpecies(curSpecies);
+
+                // Mark the best pressures for hovering over in game
+                foreach (var traversal in miche.TraversalsTerminatingInSpecies(curSpecies))
+                {
+                    results.results[curSpecies].AddBestPressuresResults(patch, traversal.Select(x => x.Pressure).ToList());
+                }
+            }
+        }
+
+        FillEmptyMiches(foreignSpecies, results, patch, cache);
     }
 
-    public static void FillEmptyMiches(IEnumerable<Species> allSpecies, RunResults results, Patch patch, SimulationCache cache)
+    public static void FillEmptyMiches(IEnumerable<Species> foreignSpecies, RunResults results, Patch patch, SimulationCache cache)
     {
         // if there are any empty miches, try out other species to fill the gap
         foreach (var emptyMiche in results.MicheByPatch[patch].TraversalsTerminatingInSpecies(null))
         {
-            FillEmptyMiche(emptyMiche, allSpecies, results, patch, cache);
+            FillEmptyMiche(emptyMiche, foreignSpecies, results, patch, cache);
         }
     }
 
-    private static void FillEmptyMiche(IEnumerable<Miche> emptyMiche, IEnumerable<Species> allSpecies, RunResults results, Patch patch, SimulationCache cache)
+    private static void FillEmptyMiche(IEnumerable<Miche> emptyMiche, IEnumerable<Species> foreignSpecies, RunResults results, Patch patch, SimulationCache cache)
     {
         var miche = results.MicheByPatch[patch];
 
         GD.Print("Searching to fill empty path: " + String.Join(",", emptyMiche.Select(x => x.Name)));
-        foreach (var curSpecies in allSpecies)
-        {
-            var variants = ModifyExistingSpecies.ViableVariants(results, curSpecies, patch, new PartList(curSpecies), cache, emptyMiche.Select(x => x.Pressure).ToList());
 
-            if (variants.Count() > 0)
+        var pointer = emptyMiche.Last();
+        // This should always be null at this stage
+        var finalSpecies = emptyMiche.Last().Occupant;
+
+        while (pointer.Parent != null && finalSpecies == null)
+        {
+            pointer = pointer.Parent;
+
+            foreach (var curSpecies in pointer.AllOccupants())
             {
-                // This may do nothing or be overwritten, and that's ok
-                emptyMiche.Last().InsertSpecies(variants.First());
-                results.AncestorDictionary.Add(variants.First(), curSpecies);
+                var variants = ModifyExistingSpecies.ViableVariants(results, curSpecies, patch, new PartList(curSpecies), cache, emptyMiche.Select(x => x.Pressure).ToList());
+
+                if (variants.Count() > 0)
+                {
+                    // This may do nothing or be overwritten, and that's ok
+                    emptyMiche.Last().InsertSpecies(variants.First());
+                    results.AncestorDictionary.Add(variants.First(), curSpecies);
+                }
             }
+
+            finalSpecies = emptyMiche.Last().Occupant;
         }
 
-        var finalSpecies = emptyMiche.Last().Occupant;
+        // If no native species rose to the challange, then a foreign species gets a chance
+        if (finalSpecies == null)
+        {
+            foreach (var curSpecies in foreignSpecies)
+            {
+                var variants = ModifyExistingSpecies.ViableVariants(results, curSpecies, patch, new PartList(curSpecies), cache, emptyMiche.Select(x => x.Pressure).ToList());
+
+                if (variants.Count() > 0)
+                {
+                    // This may do nothing or be overwritten, and that's ok
+                    emptyMiche.Last().InsertSpecies(variants.First());
+                    results.AncestorDictionary.Add(variants.First(), curSpecies);
+                }
+            }
+        }
+        
 
         if (finalSpecies != null)
         {
@@ -95,11 +139,9 @@ class PullSpeciesForPatch : IRunStep
         }
     }
 
-    private IEnumerable<Species> CandiateSpecies()
+    private IEnumerable<Species> NeighboringSpecies()
     {
         var retval = Patch.Adjacent.SelectMany(x => x.SpeciesInPatch.Keys).ToList();
-
-        retval.AddRange(Patch.SpeciesInPatch.Select(x => x.Key));
 
         return retval;
     }
