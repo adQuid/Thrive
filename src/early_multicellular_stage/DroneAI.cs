f66234f43f29b0171e6d67d11072a3bdbf4126fa
adQuid <28126232+adQuid@users.noreply.github.com>
2023-02-26 13:12:28 -0500

    engulf when close

--- src/early_multicellular_stage/DroneAI.cs
+++ src/early_multicellular_stage/DroneAI.cs
@@ -19,10 +19,48 @@ class DroneAI
         var retval = new DroneAIResponse();
         retval.Drone = microbe;
 
-        retval.State = Microbe.MicrobeState.Engulf;
-        //DebugFlash(microbe);
+        if (microbe.CellTypeProperties.MembraneType.CellWall)
+        {
+            var thingIMightWantToEat = NearestEngulfableThing(data);
+
+            if (thingIMightWantToEat != null && DistanceFromMe((Vector3)thingIMightWantToEat) < 15.0f)
+            {
+                retval.State = Microbe.MicrobeState.Engulf;
+            }
+        }
 
         return retval;
+    }
+
+    private Vector3? NearestEngulfableThing(MicrobeAICommonData data)
+    {
+        float lowestDistance = 9999.0f;
+        Vector3? target = null;
+
+        foreach (var chunk in data.AllChunks)
+        {
+            if (DistanceFromMe(chunk.GlobalTransform.origin) < lowestDistance)
+            {
+                target = chunk.GlobalTransform.origin;
+                lowestDistance = DistanceFromMe(chunk.GlobalTransform.origin);
+            }
+        }
+
+        foreach (var microbe in data.AllMicrobes)
+        {
+            if (DistanceFromMe(microbe.GlobalTransform.origin) < lowestDistance)
+            {
+                target = microbe.GlobalTransform.origin;
+                lowestDistance = DistanceFromMe(microbe.GlobalTransform.origin);
+            }
+        }
+
+        return target;
+    }
+
+    private float DistanceFromMe(Vector3 target)
+    {
+        return (target - microbe.Translation).LengthSquared();
     }
 
     private void DebugFlash()
